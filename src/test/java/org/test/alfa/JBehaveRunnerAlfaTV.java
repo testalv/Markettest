84f3db820d521909125a79b0e0a1f8e8e2297b3b
testalv <epsy2@yandex.ru>
2016-12-06 03:37:44 +0300

    Add files via upload

--- src/test/java/org/test/alfa/JBehaveRunnerAlfaTV.java
+++ src/test/java/org/test/alfa/JBehaveRunnerAlfaTV.java
@@ -2,51 +2,51 @@ package org.test.alfa;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.parsers.RegexPrefixCapturingPatternParser;
-import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
-import org.jbehave.core.steps.PrintStreamStepMonitor;
-import org.testng.annotations.Test;
+import org.jbehave.core.steps.Steps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.*;
 
 /**
  * Created by Vladimir on 05.12.2016.
  */
 
 public class JBehaveRunnerAlfaTV extends JUnitStories {
-    public JBehaveRunnerAlfaTV() {
-        super();
-        this.configuredEmbedder().candidateSteps().add(new AlfaTestStepsTV());
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration()
+                .useStoryLoader(
+                        new LoadFromClasspath(this.getClass().getClassLoader()))
+                .useStoryReporterBuilder(
+                        new StoryReporterBuilder()
+                                .withDefaultFormats()
+                                .withFormats(Format.HTML, Format.CONSOLE)
+                                .withRelativeDirectory("jbehave-report"));
     }
-    private final CrossReference xref = new CrossReference();
-
-    //    @Override
-    public Configuration configuration = new MostUsefulConfiguration()
-            .useStoryLoader(new LoadFromClasspath(this.getClass()))
-            .useStoryReporterBuilder(
-                    new StoryReporterBuilder()
-                            .withFormats(STATS, CONSOLE, TXT, HTML, XML)
-                            .withFailureTrace(true).withCrossReference(xref))
-            .useStepPatternParser(new RegexPrefixCapturingPatternParser("%"))
-            .useStepMonitor(new PrintStreamStepMonitor());
 
     @Override
     public InjectableStepsFactory stepsFactory() {
-        return new InstanceStepsFactory(configuration, new AlfaTestStepsTV());
+        ArrayList<Steps> stepFileList = new ArrayList<Steps>();
+        stepFileList.add(new AlfaTestStepsTV());
+
+        return new InstanceStepsFactory(configuration(), stepFileList);
     }
 
-    @Test
+    @Override
     protected List<String> storyPaths() {
-        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()),
-                "**/AlfaTV.story", "");
+        return new StoryFinder().
+                findPaths(CodeLocations.codeLocationFromClass(
+                        this.getClass()),
+                        Arrays.asList("**/AlfaTV.story"),
+                        Arrays.asList(""));
     }
 }
